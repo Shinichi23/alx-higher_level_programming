667ca5b62761fc928e3318f5b5efa76dbc2a183a
Shinichi23 <taha-chaoui@hotmail.com>
2022-11-29 14:18:22 -0800

    Correction

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(abs(number) % 10, end="")
-    return (abs(number) % 10)
+    number = f'{number:d}'
+    print(int(number[-1:]+number[-1:]))
