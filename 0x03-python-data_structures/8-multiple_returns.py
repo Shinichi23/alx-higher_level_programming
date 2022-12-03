016c328a535dd0180445ccaedb52496932923f6b
Shinichi23 <taha-chaoui@hotmail.com>
2022-12-03 10:43:14 -0800

    Correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    a = len(sentence)
-    b = sentence[0]
     if sentence = "":
         return (0, None)
-    return (a, b)
+    return (len(sentence), sentence[0])
