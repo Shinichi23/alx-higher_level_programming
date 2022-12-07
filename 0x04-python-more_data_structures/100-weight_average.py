a351dd5a4570fa95bf2cd917f56ccbb098920bec
Shinichi23 <taha-chaoui@hotmail.com>
2022-12-07 14:18:05 -0800

    Correction

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num1 = 0
+    num2 = 0
+
+    if tup in my_list:
+        num1 += tup[0] * tup[1]
+        num2 += tup[1]
+
+    return (num1 / num2)
