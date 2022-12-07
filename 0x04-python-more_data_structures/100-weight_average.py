7ab0f9e0889d563dae7a588f62a81344b91f67e8
Shinichi23 <taha-chaoui@hotmail.com>
2022-12-07 14:20:40 -0800

    Correction

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,11 +3,11 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
 
-    num1 = 0
-    num2 = 0
+    num = 0
+    den = 0
 
-    if tup in my_list:
-        num1 += tup[0] * tup[1]
-        num2 += tup[1]
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
 
-    return (num1 / num2)
+    return (num / den)
